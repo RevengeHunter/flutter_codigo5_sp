5c1ee11bbe8cb00e4c1ee3757c5399f867f776da
RevengeHunter <cdtvladimir0992@gmail.com>
2022-05-11 20:52:42 -0500

    Use of drawer to build a vertical menu.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -48,7 +48,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<int> getNumberMandarina() async {
-    return Future.delayed(Duration(seconds: 3),(){
+    return Future.delayed(Duration(seconds: 3), () {
       return 100;
     });
   }
@@ -56,20 +56,78 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: FutureBuilder(
-          future: getNumberMandarina(),
-          builder: (BuildContext context, AsyncSnapshot snap) {
-            if (snap.hasData) {
-              int numero = snap.data;
-              return Center(
-                child: Text("$numero"),
-              );
-            }
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          },
+      appBar: AppBar(
+        backgroundColor: Colors.pinkAccent,
+        title: Text("Home"),
+      ),
+      drawer: Drawer(
+        elevation: 1,
+        child: Column(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://images.pexels.com/photos/1762973/pexels-photo-1762973.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+                ),
+              ),
+              child: Container(
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CircleAvatar(
+                          radius: 40.0,
+                          foregroundImage: NetworkImage(
+                            "https://images.pexels.com/photos/320014/pexels-photo-320014.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8.0,
+                        ),
+                        Text(
+                          "Pedro Catbell",
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 4.0,
+                        ),
+                        Text(
+                          "Administrador",
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.white70,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("My Profile"),
+            ),
+            ListTile(
+              leading: Icon(Icons.file_copy),
+              title: Text("Portfolio"),
+            ),
+            ListTile(
+              leading: Icon(Icons.lock),
+              title: Text("Change Password"),
+            ),
+            Divider(thickness: 0.6,indent: 22,endIndent: 22,),
+            ListTile(
+              leading: Icon(Icons.exit_to_app,),
+              title: Text("Log Out"),
+            ),
+          ],
         ),
       ),
     );
